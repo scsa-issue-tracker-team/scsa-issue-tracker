aeda2c95208819bba6a6b56eccebe0c73dd9ef3e
wonnowone <harmonace10@gmail.com>
2026-05-20 12:26:59 +0900

    error handler commit

---
+++ backend/src/main/java/com/scsa/issuetracker/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.scsa.issuetracker.common.exception;
+
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNotFound(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Map.of("error", ex.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleGeneric(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of("error", "예기치 못한 에러 발생"));
+    }
+}
+
+//C:\Users\student\IdeaProjects\scsa-issue-tracker\backend\src\main\java\com\scsa\issuetracker\common
