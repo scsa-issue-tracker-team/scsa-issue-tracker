d54e76b3383fa778fa42b4ff44f172dd73c6b024
Lee Ji Eon <ez_on@naver.com>
2026-05-20 14:15:32 +0900

    Minor Fix : Project Repo Annotation

--- backend/src/main/java/com/scsa/issuetracker/project/repository/ProjectRepository.java
+++ backend/src/main/java/com/scsa/issuetracker/project/repository/ProjectRepository.java
@@ -2,6 +2,8 @@ package com.scsa.issuetracker.project.repository;
 
 import com.scsa.issuetracker.project.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 }
